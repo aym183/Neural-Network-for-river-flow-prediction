38ce15f0cb77ca5fbc50eb39e70a5a7028e3642b
Ayman <ayman.ali1302@gmail.com>
2022-03-28 20:04:27 +0100

    Updated MLP with destandardized outputs

--- MLP.py
+++ MLP.py
@@ -9,7 +9,7 @@ np.random.seed(95)
 class Neural_Network(object):
   
 
-    def __init__(self, inputs=7, outputs=1, hidden_layers=[3, 1]):
+     def __init__(self, inputs, outputs, hidden_layers):
         
         ''' Constructor that takes the number of inputs, hidden layers and number of outputs'''
 
@@ -22,61 +22,65 @@ class Neural_Network(object):
 
         
         # activations list created with zeros added to each index
-        activations = []
+        activ_operations = []
         for i in range(len(layers)):
             a = np.zeros(layers[i])
-            activations.append(a)
+            activ_operations.append(a)
         
         
         # weights are randomly created bwlpw
         new_weights = []
         # derivatives list created with zeros added to each index
-        derivatives = []
+        deriv_operations = []
         for i in range(len(layers) - 1):
             
             weight = np.random.rand(layers[i], layers[i + 1])
             d = np.zeros((layers[i], layers[i + 1]))
             
             new_weights.append(weight)
-            derivatives.append(d)
+            deriv_operations.append(d)
             
-        self.activations = activations
+        self.activ_operations = activ_operations
         self.weights = new_weights
-        self.derivatives = derivatives
+        self.deriv_operations = deriv_operations
     
 
     def forward_propagation(self, inputs):
+
         '''This method goes about the process of forward propogation that returns predicted output'''
 
         # the input passed to the the method is added
-        activations = inputs
-        self.activations[0] = activations
+        activ_operations = inputs
+        self.activ_operations[0] = activ_operations
         
         for i, w in enumerate(self.weights):
             
-            # dot product of inputs and weights
-            net_inputs = np.dot(activations, w)
+            # calculate matrix multiplication between previous activation and weight matrix
+            net_inputs = np.dot(activ_operations, w)
 
             # Calling of activation function (i.e. tanh)
-            activations = self._tanh(net_inputs)
-            self.activations[i + 1] = activations
+            activ_operations = self._sigmoid(net_inputs)
+            
+            self.activ_operations[i + 1] = activ_operations
 
-        return activations
+        return activ_operations
 
 
     def back_propagation(self, error):
         '''This method goes about the process of back propogation'''
         
-        for i in reversed(range(len(self.derivatives))):
+        for i in reversed(range(len(self.deriv_operations))):
 
-            activations = self.activations[i+1]
+            activ_operations = self.activ_operations[i+1]
             
             # Calling of derivative of activation function (i.e. tanh)
-            delta = error * self._tanh(activations, deriv = True)        
+            delta = error * self._sigmoid(activ_operations, deriv = True)
+
+        
             delta_re = delta.reshape(delta.shape[0], -1).T
-            current_activations = self.activations[i]
+            current_activations = self.activ_operations[i]
             current_activations = current_activations.reshape(current_activations.shape[0],-1)
-            self.derivatives[i] = np.dot(current_activations, delta_re)
+            self.deriv_operations[i] = np.dot(current_activations, delta_re)
 
             # backpropogation of the next error
             error = np.dot(delta, self.weights[i].T)
@@ -88,14 +92,14 @@ class Neural_Network(object):
         # this loop updates the weights by changing gradient constantly
         for i in range(len(self.weights)):
             new_weights = self.weights[i]
-            derivatives = self.derivatives[i]
-            new_derivative = self.derivatives[i-1]
+            deriv_operations = self.deriv_operations[i]
+            new_derivative = self.deriv_operations[i-1]
         
-            new_weights+= derivatives * learningRate
+            new_weights+= deriv_operations * learningRate
             
-            # MOMENTUM OPTIMIZER 
+            # MOMENTUM OPTIMIZER -> used in final algorithm (mentioned why in report)
             
-            new_weights = new_weights - (derivatives * learningRate) + (momentum * new_derivative)
+            new_weights = new_weights - (deriv_operations * learningRate) + (momentum * new_derivative)
 
     # Activation functions -> when deriv = True, the derivatives of the functions are returned
 
@@ -128,60 +132,106 @@ class Neural_Network(object):
         """ Function that goes about conducting Mean Squared Error """
         
         # calculates subtraction of target and output
-        return np.average((target - output) ** 2)
 
-    def train(self, inputs, targets, val_inputs, val_outputs, epochs, learning_rate):
+        return np.average((target - output) ** 2)
+        
+        
+    
+
+    def train(self, inputs, targets, val_inputs, val_outputs, test_inputs, test_outputs, epochs, learning_rate):
         """ Function that trains the algorithm """
         
         y_args = []
         y_args2 = []
         
+        destand_outputs = []
+        test_destand_outputs = []
+        train_destand_outputs = []
+        
+        # Destandardizing of outputs of training, validation and test sets
+        for i in range(len(val_inputs)):
+                destand_outputs.append([(((val_outputs[i][0] - 0.1)*(max_skelton - min_skelton))/ 0.3) + min_skelton])
+        
+        for i in range(len(targets)):
+                train_destand_outputs.append([(((targets[i][0] - 0.1)*(max_skelton - min_skelton))/ 0.3) + min_skelton])
+                
+                
+        for i in range(len(test_outputs)):
+                test_destand_outputs.append([(((test_outputs[i][0] - 0.1)*(max_skelton - min_skelton))/ 0.3) + min_skelton])
+
+
+        # TRAINING TAKES PLACE HERE
+
         for i in range(epochs):
             sum_errors = 0
             sum_errors2 = 0
             
-            # In the first loop, training set error is taken
+            # In each loop, training input at each index is taken
             for j, input in enumerate(inputs):
                 target = targets[j]
 
                 # Output is taken by running forward propogation and error is calculated
                 
-                output = self.forward_propagation(input)
+                output = self.forward_propagation(input)  
+
                 error = target - output
                 self.back_propagation(error)
                 self.gradient_descent(learning_rate)
 
-                # adding all outputs of MSE function to use later to calculate error
-                sum_errors += self._mse(target, output)
+                # Destandardizing of output
+
+                output[0] = (((output[0] - 0.1)*(max_skelton - min_skelton))/ 0.3) + min_skelton 
+    
+                
+                # adding all outputs of MSE function to use later to calculate error with destandardized values
+                sum_errors += self._mse(train_destand_outputs[j][0], output[0])
 
             # After epoch is completed, training error is reported
             error = sum_errors / len(items)
+
+            # RMSE
+            root_error = math.sqrt(sum_errors / len(items))
             y_args.append(sum_errors / len(items))
-      
-            
-            # In the second loop, validation set error is taken
+            sum_errors = 0
+                
+        
+            # In each loop, validation input at each index is taken
             for j, input in enumerate(val_inputs):
                 
                 target = val_outputs[j]
+                
                 output = self.forward_propagation(input)
-                error2 = target - output
-                sum_errors += self._mse(val_outputs, output)
 
-           
-            error2 = sum_errors / 290
-   
-            # Error rate for both training and validation set is displayed
+                # Destandardizing of output
+                output[0] = (((output[0] - 0.1)*(max_skelton - min_skelton))/ 0.3) + min_skelton 
+        
+                sum_errors += self._mse(destand_outputs[j][0], output[0])
     
-            print("Error: {}, Validate Error: {} at epoch {}".format(round(error,6), round(error2,6), i+1))
-
-            y_args2.append(sum_errors / len(items))
+            error2 = sum_errors / 290
+            y_args2.append(sum_errors / 290)
+    
+            sum_errors = 0
+        
+            # In each loop, test input at each index is taken
+            for j, input in enumerate(test_inputs):
+                
+                target = test_outputs[j]
             
+                
+                output = self.forward_propagation(input)
+               
+                # Destandardizing of output
+                output[0] = (((output[0] - 0.1)*(max_skelton - min_skelton))/ 0.3) + min_skelton 
+                    
+                sum_errors += self._mse(test_destand_outputs[j][0], output[0])
+    
+            error3 = sum_errors / 290
+            
+            sum_errors = 0
 
-        plt.plot(y_args)
-        plt.plot(y_args2)
-        plt.legend(['Training Set', 'Validation Set'])
-        plt.savefig('MSE.png', transparent = True)
-        plt.show()
+            # RMSE Error rate for training, validation and test set is displayed
+
+            print("Error: {}, Validate Error: {}, Test Error: {} at epoch {}".format(round(math.sqrt(error),6),round(math.sqrt(error2),6) ,round(math.sqrt(error3),6), i+1))
 
 
         print("Training complete!")
@@ -200,15 +250,26 @@ if __name__ == "__main__":
     items2 = np.array((val_input_list))
     targets2 =  np.array((val_output_list))
     
+    input = np.array((test_input_list))
+    target = np.array((test_output_list))
     # training parameters (7 inputs, 1 output and 7 neurons in one hidden layer)
     mlp = Neural_Network(7, 1, [7])
-    mlp.train(items, targets, items2, targets2, 75, 0.01)
+    mlp.train(items, targets, items2, targets2, input, target, 750, 0.7)
 
-    # test data
-    input = np.array(test_input_list) 
-    target = test_output_list
 
     # returns predicted output
     output = mlp.forward_propagation(input)
     
-    print("Output -> {}".format(output))
+#  Destandardizing of Expected and Predicted output to display
+
+    new_test_output = []
+    for i in range(len(output)):
+        
+        new_test_output.append((((test_output_list[i][0] - 0.1)*(max_skelton - min_skelton))/ 0.3) + min_skelton)
+        output[i][0] = (((output[i][0] - 0.1)*(max_skelton - min_skelton))/ 0.3) + min_skelton
+    
+
+    for i in range(len(output)):
+        print("Actual Output -> {} Output -> {}".format(round(new_test_output[i], 6), round(output[i][0], 6)))
+        
+        
